74e362a47ab1dd8660e3615b56561c90f7a2318d
PabloJL <jup_jim_7@hotmail.com>
2024-08-06 12:39:23 -0600

    Data fetching

--- App.js
+++ App.js
@@ -1,33 +1,37 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import {
   StyleSheet,
   Text,
   View,
-  Image,
-  Button,
-  TouchableHighlight,
   Pressable,
+  Image,
+  ScrollView,
 } from "react-native";
+import { getLatestGames } from "./lib/metacritic";
 
-const icon = require("./assets/icon.png");
 export default function App() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    getLatestGames().then((games) => {
+      setGames(games);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      <Pressable
-        underlayColor={"#09f"}
-        onPress={() => alert("Hola")}
-        style={{
-          width: 200,
-          height: 200,
-          backgroundColor: "blue",
-          borderRadius: 100,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text style={{ color: "white" }}>Pulsa aqui!</Text>
-      </Pressable>
+      <ScrollView>
+        {games.map((game) => (
+          <View key={game.slug} style={styles.card}>
+            <Image source={{ uri: game.image }} style={styles.image} s />
+            <Text style={styles.title}>{game.title}</Text>
+            <Text style={styles.description}>{game.description}</Text>
+            <Text style={styles.score}>{game.score}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -38,5 +42,29 @@ const styles = StyleSheet.create({
     backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
+  },
+  card: {
+    marginBottom: 42,
+  },
+  image: {
+    width: 107,
+    height: 147,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+    marginTop: 10,
+  },
+  description: {
+    fontSize: 16,
+    color: "#eee",
+  },
+  score: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "green",
+    marginBottom: 10,
   },
 });
