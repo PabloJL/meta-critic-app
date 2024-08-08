601303f0999ee2b386c08d594aa39718754163e4
PabloJL <jup_jim_7@hotmail.com>
2024-08-08 13:46:45 -0600

    Added tabs

--- app/[gameslug].js
+++ app/[gameslug].js
@@ -1,19 +1,55 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, View, ActivityIndicator, ScrollView, Image } from "react-native";
 import Screen from "../components/Screen";
 import { useLocalSearchParams } from "expo-router";
-
+import { Stack } from "expo-router";
+import { useEffect, useState } from "react";
+import { getGameDetails } from "../lib/metacritic";
+import Score from "../components/Score";
 export default function Detail() {
   const { gameslug } = useLocalSearchParams();
+  const [gameInfo, setGameInfo] = useState(null);
+
+  useEffect(() => {
+    if (gameslug) {
+      getGameDetails(gameslug).then(setGameInfo);
+    }
+  }, [gameslug]);
+
   return (
     <Screen>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: "#ffee00" },
+          headerTintColor: "black",
+          headerLeft: () => {},
+          headerTitle: gameslug,
+          headerRight: () => {},
+        }}
+      />
       <View>
-        <Text className="text-white font-bold mb-8 text-2xl">
-          Game Details {gameslug}
-        </Text>
-        <Link href="/" className="text-blue-500">
-          Home
-        </Link>
+        {gameInfo === null ? (
+          <ActivityIndicator color={"#fff"} size={"large"} />
+        ) : (
+          <ScrollView>
+            <View className="justify-center items-center text-center">
+              <Image
+                className="mb-4 rounded"
+                source={{ uri: gameInfo.img }}
+                style={{ width: 214, height: 294 }}
+              />
+
+              <Score score={gameInfo.score} maxScore={100} />
+
+              <Text className="text-white text-center font-bold  text-xl">
+                {gameInfo.title}
+              </Text>
+              <Text className="text-white/70 mt-4 text-left mb-8 text-base">
+                {gameInfo.description}
+              </Text>
+            </View>
+          </ScrollView>
+        )}
       </View>
     </Screen>
   );
